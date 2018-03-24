3d7f4d31eca0c74a51b65d4a734914f86a4feaf3
Bernat77 <bernatsalleraslopez@hotmail.com>
2018-03-24 03:09:44 +0100

    1

--- Sesion/src/sesion/IA1.java
+++ Sesion/src/sesion/IA1.java
@@ -20,10 +20,26 @@ public class IA1 extends IA0 {
     @Override
     public Movimiento movimiento(Tablero tablero) {
 
+        int[][] casillas = tablero.getCasillas();
         Movimiento movi = new Movimiento();
-        int x, y;
-        x = 0;
-        y = 0;
+        int x = 0;
+        int y = 0;
+
+        //Busca una casilla vacía y guarda sus coordenadas.
+        
+        buscar:     //etiquetamos el bucle grande
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                if (tablero.getCasilla(i, j) == 0) {
+                    x = i;
+                    y = j;
+                    break buscar;       //para romperlo en cuanto encuentre la primera casilla vacía.
+
+                }
+            }
+        }
+
+//Hará una comparación entre la casilla vacía que ha guardado y las demás casillas disponibles. Si hay una de mayor peso se quedará con esa.
         for (int i = 0; i < pesos.length; i++) {
             for (int j = 0; j < pesos[i].length; j++) {
                 if (pesos[x][y] < pesos[i][j] && tablero.getCasilla(i, j) == 0) {
@@ -32,7 +48,7 @@ public class IA1 extends IA0 {
                 }
             }
 
-        }        
+        }
         movi.setRow(x);
         movi.setCol(y);
         movi.setBlancas(false);
@@ -40,11 +56,4 @@ public class IA1 extends IA0 {
         return movi;
     }
 
-    
-    
-    
-    
-    
-    
-    
 }
